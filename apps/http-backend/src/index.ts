0ef854feba8a03eb3fad24e6f4e081fe97708c23
RamanKurai <ramankurai27@gmail.com>
2025-08-16 13:38:15 +0530

    Add prisma table and changes in server

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,1 +1,18 @@
-import { Express } from "express";
+import express from "express";
+import jwt from "jsonwebtoken"
+const app = express();
+
+app.post("/signup" , (req , res) => {
+
+})
+
+app.post("/signin" , (req , res) => {
+    
+})
+
+app.post("/room" , (req , res) => {
+    
+})
+
+
+app.listen(3001)
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,3 @@
-import { WebSocket } from "ws";
+import { WebSocket , WebSocketServer } from "ws";
+
+const wss = new WebSocketServer ({port : 8080})
