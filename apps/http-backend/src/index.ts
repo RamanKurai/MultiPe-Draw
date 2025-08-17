5b2c8833ce958f6a2150689a3b37670a3afb91db
RamanKurai <ramankurai27@gmail.com>
2025-08-17 23:11:27 +0530

    feat: update package.json and add common package with schemas

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import jwt from "jsonwebtoken"
+import { prismaClient } from "@repo/db/client";
+
 const app = express();
 
-app.post("/signup" , (req , res) => {
-
+app.post("/signup" , (req , res) => { 
+  
 })
 
 app.post("/signin" , (req , res) => {
-    
+
 })
 
 app.post("/room" , (req , res) => {
     
 })
-
 
 app.listen(3001)
