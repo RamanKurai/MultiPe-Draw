ea82b7dbce410d47b96162676eb1a488cd6ae3aa
RamanKurai <ramankurai27@gmail.com>
2025-09-01 11:46:23 +0530

    feat: enhance drawing functionality with shape management and preview on canvas

--- apps/excalidraw/draw/index.ts
+++ apps/excalidraw/draw/index.ts
@@ -1,36 +1,90 @@
-export default function initDraw (canvas : HTMLCanvasElement) {
-            const ctx = canvas.getContext("2d");
+type Shape = {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+} | {
+      type: "circle";
+      centerX: number;
+      centerY: number;
+      radius: number;
+};
 
-            if (!ctx) {
-                return
-            }
+export default function initDraw(canvas: HTMLCanvasElement) {
+  function getMousePos(canvas: HTMLCanvasElement, e: MouseEvent) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    };
+  }
 
-            let clicked = false;
-            let startX = 0;
-            let startY = 0;
+  const ctx = canvas.getContext("2d");
+  let existingShapes: Shape[] = [];
 
-            canvas.addEventListener("mousedown", (e) => {
-                clicked = true
-                startX = e.clientX
-                startY = e.clientY
-            })
+  if (!ctx) {
+    return;
+  }
 
-            canvas.addEventListener("mouseup", (e) => {
-                clicked = false
-                console.log(e.clientX)
-                console.log(e.clientY)
-            })            
+  ctx.fillStyle = "rgba(0, 0, 0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-            canvas.addEventListener("mousemove", (e) => {
-                console.log(MouseEvent , "rectangle moving")
-                if (clicked) {
-                    const width = e.clientX - startX;
-                    const height = e.clientY - startY;
-                    ctx.clearRect(0, 0, canvas.width, canvas.height)
-                    ctx.fillStyle = "rgba(0, 0, 0)"
-                    ctx.fillRect(0 , 0 , canvas.width , canvas.height)
-                    ctx.strokeStyle = "rgba(255, 255, 255)"
-                    ctx.strokeRect(startX, startY, width, height);
-                }
-            })   
+  let clicked = false;
+  let startX = 0;
+  let startY = 0;
+
+  canvas.addEventListener("mousedown", (e) => {
+    clicked = true;
+    startX = e.clientX;
+    startY = e.clientY;
+  });
+
+  canvas.addEventListener("mouseup", (e) => {
+  clicked = false;
+  const { x, y } = getMousePos(canvas, e);
+  const width = x - startX;
+  const height = y - startY;
+
+  existingShapes.push({
+    type: "rect",
+    x: startX,
+    y: startY,
+    width,
+    height,
+  });
+
+  clearCanvas(existingShapes, canvas, ctx);
+});
+
+  canvas.addEventListener("mousemove", (e) => {
+  if (clicked) {
+    const { x, y } = getMousePos(canvas, e);
+    const width = x - startX;
+    const height = y - startY;
+
+    clearCanvas(existingShapes, canvas, ctx);
+
+    // draw preview rect
+    ctx.strokeStyle = "rgba(255,255,255)";
+    ctx.strokeRect(startX, startY, width, height);
+  }
+});
+
+function clearCanvas(
+  existingShapes: Shape[],
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(0, 0, 0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  existingShapes.map((shape) => {
+    if (shape.type == "rect") {
+      ctx.strokeStyle = "rgba(255, 255, 255)";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+    }
+  });
+}
 }
